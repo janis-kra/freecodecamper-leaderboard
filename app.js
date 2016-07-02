852fe7ea4f0f06270750dc4a79e68972543ab37f
Janis Krasemann <krasemann.janis@gmail.com>
2016-07-02 10:46:10 +0200

    Remove Radium for now, add Leaderboard and Footer components

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const top100Uri = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+const top100AlltimeUri = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+
+const Leaderboard = () => (
+  <div className='leaderboard'>
+    <h2>Leaderboard</h2>
+  </div>
+);
+
+const Footer = () => (
+  <div className='footer'>
+    <h4>by <a href='https://github.com/janis-kra'>Janis Krasemann</a></h4>
+  </div>
+);
+
 const App = () => {
   return (
     <div>
-      <h1>freecodecamper-leaderboard</h1>
+      <Leaderboard />
+      <Footer />
     </div>
   );
 };
