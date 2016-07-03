d0d5be1017c10fe6b21f44f426fdacf9094039d1
Janis Krasemann <krasemann.janis@gmail.com>
2016-07-03 17:01:49 +0200

    Use mobx for handling the app state, create a new `User` component that displays one single user

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const top100Uri = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
-const top100AlltimeUri = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+// const top100AlltimeUri = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
 
-const Leaderboard = () => (
-  <div className='leaderboard'>
-    <h2>Leaderboard</h2>
+const state = mobx.observable({
+  top100: [],
+  top100Alltime: []
+});
+
+const User = ({ username, alltime, recent, img }) => (
+  <div className='user'>
+    <div className='username'>
+      { username }
+    </div>
+    <div className='alltime'>
+      { alltime }
+    </div>
+    <div className='recent'>
+      { recent }
+    </div>
+    <img src={ img } className='img'/>
   </div>
 );
+
+const Leaderboard = mobxReact.observer(() => (
+  <div className='leaderboard'>
+    <h2>Leaderboard</h2>
+    { state.top100.map((user) => <User {...user} />)}
+  </div>
+));
 
 const Footer = () => (
   <div className='footer'>
@@ -13,20 +34,20 @@ const Footer = () => (
   </div>
 );
 
-const App = () => {
+const App = mobxReact.observer(() => {
   fetch(top100Uri).then((result) => result.json()).then((top100) => {
-    // TODO: Set state (possibly with mobx or sth. similar)
+    state.top100 = top100;
   });
-  fetch(top100AlltimeUri).then((result) => result.json()).then((top100) => {
-    // TODO: Set state (possibly with mobx or sth. similar)
-  });
+  // fetch(top100AlltimeUri).then((result) => result.json()).then((top100) => {
+  //   state.top100Alltime = top100;
+  // });
   return (
     <div>
       <Leaderboard />
       <Footer />
     </div>
   );
-};
+});
 
 ReactDOM.render(
   <App />,
